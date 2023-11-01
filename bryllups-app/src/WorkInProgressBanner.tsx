adb6a94b1c8cb070b23d860672321dc7222b06cb
Magnus Kråbøl <magnus.krabol@kantega.no>
2023-11-01 20:29:10 +0100

    wip banner close to design

--- bryllups-app/src/WorkInProgressBanner.tsx
+++ bryllups-app/src/WorkInProgressBanner.tsx
@@ -1,54 +1,69 @@
 import styled from 'styled-components';
 import logo from './assets/logo.png';
 
-import flower1 from './assets/Rectangle 27.png';
-import flower2 from './assets/Rectangle 9.png';
+import flower1 from './assets/peoni.png';
+import flower2 from './assets/peoni2.png';
 
 const WorkInProgressDiv = styled.div`
     display: flex;
-    justify-content: center;
     flex-direction: column; 
     align-items: center;
-    margin-top: 3rem;
-    gap: 50px;
+    margin-top: 5rem;
+    gap: 100px;
 `;
 
 const Logo = styled.img`
     object-fit: cover;
-    position: relative;
-    width: 60%;
+    width: 80%;
     max-width: 500px;
+`;
+
+const Info = styled.span`
+    text-align: center;
+    color: white;
+    text-transform: uppercase;
+    font-size: 1.5rem;
+    font-weight: 400;
 `;
 
 const Flowers = styled.div`
     display: flex;
     justify-content: space-between;
+    bottom: 0;
     width: 100%;
+    position: absolute;
 `;
 
-const FlowerImg = styled.img`
-    width: 100%;
-    heigh: 100%;
-    max-width: 500px;
-`;
+const FlowerImgLeft = styled.img`
+    position: relative;    
+    bottom: -50px;
+    left: -60px;    
+    transform: rotate(55deg);
+    width: 100px;
+    max-width: 200px;
+    `;
 
-
-const Info = styled.span`
-    font-size: 1.5rem;
-    color: white;
-    text-transform: uppercase;
-`;
+const FlowerImgRight = styled.img`
+    position: relative;
+    bottom: -50px;
+    right: -60px;
+    transform: rotate(15deg);
+    width: 100px;
+    max-width: 200px;
+    `;
 
 const WorkInProgressBanner = () => {
     return (
+        <>
         <WorkInProgressDiv>
             <Logo src={logo}/>
-            <Info>Her vil det komme informasjon etter 1.desember.</Info>
-            <Flowers>
-                <FlowerImg alt="" src={flower1}/>
-                <FlowerImg alt="" src={flower2}/>
-            </Flowers>
+            <Info>HER VIL DET KOMME INFORMASJON ETTER 1.DESEMBER.</Info>
         </WorkInProgressDiv>
+        <Flowers>
+            <FlowerImgLeft alt="" src={flower2}/>
+            <FlowerImgRight alt="" src={flower1}/>
+        </Flowers>
+        </>
     );
 };
 
